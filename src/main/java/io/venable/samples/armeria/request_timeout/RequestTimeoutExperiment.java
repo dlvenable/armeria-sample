2bf911126d2610ab4284f0adbf177999e3ea59b3
David Venable <dlv@amazon.com>
2024-04-25 14:58:21 -0500

    Updates to the RequestTimeout experiment to reproduce a different error with a throttling decorator.

--- src/main/java/io/venable/samples/armeria/request_timeout/RequestTimeoutExperiment.java
+++ src/main/java/io/venable/samples/armeria/request_timeout/RequestTimeoutExperiment.java
@@ -4,20 +4,24 @@ import com.linecorp.armeria.common.AggregatedHttpRequest;
 import com.linecorp.armeria.common.HttpRequest;
 import com.linecorp.armeria.common.HttpResponse;
 import com.linecorp.armeria.common.HttpStatus;
+import com.linecorp.armeria.common.MediaType;
 import com.linecorp.armeria.common.annotation.Nullable;
 import com.linecorp.armeria.server.HttpService;
 import com.linecorp.armeria.server.RequestTimeoutException;
 import com.linecorp.armeria.server.Server;
 import com.linecorp.armeria.server.ServerBuilder;
 import com.linecorp.armeria.server.ServerErrorHandler;
+import com.linecorp.armeria.server.Service;
 import com.linecorp.armeria.server.ServiceRequestContext;
 import com.linecorp.armeria.server.SimpleDecoratingHttpService;
+import com.linecorp.armeria.server.annotation.ExceptionHandlerFunction;
 import com.linecorp.armeria.server.annotation.Post;
+import com.linecorp.armeria.server.throttling.ThrottlingRejectHandler;
+import com.linecorp.armeria.server.throttling.ThrottlingService;
+import com.linecorp.armeria.server.throttling.ThrottlingStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 import java.util.function.Function;
 
 /**
@@ -49,10 +53,12 @@ public class RequestTimeoutExperiment {
         final ServerBuilder serverBuilder = Server.builder();
         return serverBuilder.http(port)
                 //.service("/test", (ctx, req) -> HttpResponse.of("Hello, Armeria!"))
+                //.decorator("/test", SimpleDecorator.newDecorator())
+                // TODO: Remove the following line to get the 408 that we'd expect.
+                .decorator(ThrottlingService.newDecorator(ThrottlingStrategy.rateLimiting(10.0), new CustomThrottlingRejectHandler()))
                 .annotatedService("/test", new MyService())
                 //.errorHandler(new CustomServerErrorHandler())
-                .decorator(SimpleDecorator.newDecorator())
-                .requestTimeout(Duration.of(5, ChronoUnit.SECONDS))
+                //.requestTimeout(Duration.of(5, ChronoUnit.SECONDS))
                 .build();
     }
 
@@ -82,7 +88,8 @@ public class RequestTimeoutExperiment {
                 log.info("In decorator");
                 if(throwable != null) {
                     log.error("Error in decorator", throwable);
-                    return HttpResponse.of(HttpStatus.REQUEST_TIMEOUT);
+                    // Returning a different response to see if it is modified
+                    return HttpResponse.of(HttpStatus.BAD_REQUEST);
                 }
 
                 try {
@@ -108,6 +115,24 @@ public class RequestTimeoutExperiment {
         @Post
         public HttpResponse doPost(final ServiceRequestContext serviceRequestContext, final AggregatedHttpRequest aggregatedHttpRequest) throws Exception {
             return HttpResponse.of("Hello, Armeria!");
+        }
+    }
+
+    private static class CustomExceptionHandlerFunction implements ExceptionHandlerFunction {
+        @Override
+        public HttpResponse handleException(ServiceRequestContext ctx, HttpRequest req, Throwable cause) {
+            log.error("Error in decorator", cause);
+            return HttpResponse.of(HttpStatus.OK, MediaType.ANY_TYPE, "Error handled");
+        }
+    }
+
+    private static class CustomThrottlingRejectHandler implements ThrottlingRejectHandler<HttpRequest, HttpResponse> {
+
+        @Override
+        public HttpResponse handleRejected(Service<HttpRequest, HttpResponse> delegate, ServiceRequestContext ctx, HttpRequest req, @Nullable Throwable cause) throws Exception {
+            return HttpResponse.of(HttpStatus.TOO_MANY_REQUESTS, MediaType.ANY_TYPE,
+                    "Not completed"
+            );
         }
     }
 }
